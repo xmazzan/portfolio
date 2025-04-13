503b1c96553ab05617b36c4d97219dd218bf76a6
Gabriel Matos <gahsund@gmail.com>
2025-04-12 21:35:02 -0300

    initial commit

---
+++ src/app/components/content/content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  imports: [],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.scss'
+})
+export class ContentComponent {
+
+}
---
+++ src/app/components/system/system.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system',
+  imports: [],
+  templateUrl: './system.component.html',
+  styleUrl: './system.component.scss'
+})
+export class SystemComponent {
+
+}
